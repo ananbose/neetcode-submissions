52badbb612bc99622991052ed4cf38319c18c940
Anandita Bose <49714386+ananbose@users.noreply.github.com>
2026-09-19 18:32:08 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #[3,2,7,3,1], basically look for a delta that is positive
+        maxp=float('-inf')
+        for i in range(len(prices)):
+            if i+1<len(prices):
+                for j in range(i+1,len(prices)):
+                    if prices[j]-prices[i] > 0:
+                        profit = prices[j]-prices[i]
+                        maxp= max(profit,maxp)
+        return max(0,maxp)
